a610211013191539ba4503d01f9b914a2452d872
Joseph <frasier.joseph@gmail.com>
2023-03-02 09:40:57 -0600

    added post route for render demo

--- index.js
+++ index.js
@@ -6,4 +6,7 @@ app.get("/get_data", (req, res) => {
   res.send("You hit my port number");
 });
 
+app.post("/get_data", (req, res) => {
+  res.json({ name: "joe", babyIsComing: "Monday" });
+});
 app.listen(PORT, console.log(`listening on port ${PORT}`));
